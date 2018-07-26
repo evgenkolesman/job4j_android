c4c6761eff49cac124b41cac7a3c6f7b45dcee4c
Petr Arsentev <parsentev@yandex.ru>
2018-07-27 00:33:33 +0400

    2. MVC. Создания события и получение данных от пользователя.[#61529]

--- app/src/main/java/ru/job4j/exam/ExamActivity.java
+++ app/src/main/java/ru/job4j/exam/ExamActivity.java
@@ -2,12 +2,96 @@ package ru.job4j.exam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ExamActivity extends AppCompatActivity {
 
+    private final List<Question> questions = Arrays.asList(
+            new Question(
+                    1, "How many primitive variables does Java have?",
+                    Arrays.asList(
+                            new Option(1, "1.1"), new Option(2, "1.2"),
+                            new Option(3, "1.3"), new Option(4, "1.4")
+                    ), 4
+            ),
+            new Question(
+                    2, "What is Java Virtual Machine?",
+                    Arrays.asList(
+                            new Option(1, "2.1"), new Option(2, "2.2"),
+                            new Option(3, "2.3"), new Option(4, "2.4")
+                    ), 4
+            ),
+            new Question(
+                    3, "What is happen if we try unboxing null?",
+                    Arrays.asList(
+                            new Option(1, "3.1"), new Option(2, "3.2"),
+                            new Option(3, "3.3"), new Option(4, "3.4")
+                    ), 4
+            )
+    );
+
+    private int position = 0;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.custom_layout);
+    protected void onCreate(Bundle state) {
+        super.onCreate(state);
+        setContentView(R.layout.activity_exam);
+        this.fillForm();
+        Button next = findViewById(R.id.next);
+        next.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        showAnswer();
+                        position++;
+                        fillForm();
+                    }
+                }
+        );
+        Button previous = findViewById(R.id.previous);
+        previous.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        position--;
+                        fillForm();
+                    }
+                }
+        );
+    }
+
+    private void fillForm() {
+        findViewById(R.id.previous).setEnabled(position != 0);
+        findViewById(R.id.next).setEnabled(position != questions.size() - 1);
+        final TextView text = findViewById(R.id.question);
+        Question question = this.questions.get(this.position);
+        text.setText(question.getText());
+        RadioGroup variants = findViewById(R.id.variants);
+        for (int index = 0; index != variants.getChildCount(); index++) {
+            RadioButton button = (RadioButton) variants.getChildAt(index);
+            Option option = question.getOptions().get(index);
+            button.setId(option.getId());
+            button.setText(option.getText());
+        }
+    }
+
+    private void showAnswer() {
+        RadioGroup variants = findViewById(R.id.variants);
+        int id = variants.getCheckedRadioButtonId();
+        Question question = this.questions.get(this.position);
+        Toast.makeText(
+                this, "Your answer is " + id + ", correct is " + question.getAnswer(),
+                Toast.LENGTH_SHORT
+        ).show();
     }
 }
---
+++ app/src/main/java/ru/job4j/exam/Question.java
@@ -0,0 +1,63 @@
+package ru.job4j.exam;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Question {
+    private int id;
+    private String text;
+    private List<Option> options;
+    private int answer;
+
+    public Question(int id, String text, List<Option> options, int answer) {
+        this.id = id;
+        this.text = text;
+        this.options = options;
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<Option> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<Option> options) {
+        this.options = options;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Question question = (Question) o;
+        return id == question.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
